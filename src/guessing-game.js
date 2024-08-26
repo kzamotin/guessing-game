c23b3432d7d87939f1ad295227242ad31e90126f
KRYPTONITE\k.zamotin <k.zamotin@kryptonite.ru>
2024-08-27 00:12:07 +0300

    feat: fuess class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.guession =0; }
 
     setRange(min, max) {
-
+	this.min = min;
+    this.max = max;
+    this.guession = Math.round((this.min + this.max)/2)+0;
     }
 
     guess() {
-
+	return this.guession;
     }
 
     lower() {
-
+	this.max=this.guession +0;
+	this.guession = Math.round((this.min + this.max)/2)+0;
+	console.log("lower", this.min, this.max, this.guession)
+	
     }
 
     greater() {
-
+	this.min=this.guession;
+	this.guession = Math.round((this.min + this.max)/2) +0;
+	console.log("greater", this.min, this.max, this.guession)
+	
+	
     }
 }
 
